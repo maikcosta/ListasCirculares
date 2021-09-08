35e9169d4dae9a803a09216fa78437945d308340
maikcosta@gmail.com <Mk1311cts>
2021-09-08 14:33:19 -0300

    Main

--- src/main/java/one/digitalinnovation/ListaCircular.java
+++ src/main/java/one/digitalinnovation/ListaCircular.java
@@ -6,11 +6,51 @@ public class ListaCircular<T> {
     private No<T> cauda;
     private int tamanhoLista;
 
+
+    public ListaCircular() {
+        this.cauda = null;
+        this.cabeca = null;
+        this.tamanhoLista = 0;
+    }
+
+    public void add(T conteudo){
+        No<T> novoNo =   new No<>(conteudo);
+        if(this.tamanhoLista == 0){
+            this.cabeca = novoNo;
+            this.cauda = this.cabeca;
+            this.cauda.setNoProximo(cauda);
+        }else{
+            novoNo.setNoProximo(this.cauda);
+            this.cabeca.setNoProximo(novoNo);
+            this.cauda = novoNo;
+        }
+        this.tamanhoLista++;
+
+    }
+
+
+    public void remove(int index){
+        if(index >= this.tamanhoLista)
+            throw new IndexOutOfBoundsException("O indice é maior que o tamanho da lista");
+        No<T> noAuxliar = this.cauda;
+        if (index == 0){
+            this.cauda = this.cauda.getNoProximo();
+            this.cabeca.setNoProximo(this.cauda);
+        }else if(index == 1){
+            this.cauda.setNoProximo(this.cauda.getNoProximo().getNoProximo());
+        }else{
+            for(int i= 0 ; i < index ; i++ ){
+                noAuxliar = noAuxliar.getNoProximo();
+            }
+            noAuxliar.setNoProximo(noAuxliar.getNoProximo().getNoProximo());
+        }
+        this.tamanhoLista--;
+    }
+
     public T get(int index){
         return this.getNo(index).getConteudo();
 
     }
-
 
     private No<T> getNo(int index){
         if(this.isEmppty())
@@ -23,7 +63,7 @@ public class ListaCircular<T> {
         for (int i = 0; (i < index) && (noAuxiliar != null); i ++){
             noAuxiliar = noAuxiliar.getNoProximo();
         }
-            return noAuxiliar;
+        return noAuxiliar;
     }
 
     public boolean isEmppty(){
@@ -36,5 +76,16 @@ public class ListaCircular<T> {
         return this.tamanhoLista;
     }
 
+    @Override
+    public String toString() {
+        String strRetorno = "";
+        No<T> noAuxiliar = this.cauda;
+        for (int i = 0; i < this.size(); i ++){
+            strRetorno += "N[No{conteudo=" + noAuxiliar.getConteudo() + "}]---->";
+            noAuxiliar= noAuxiliar.getNoProximo();
+        }
+        strRetorno += this.size() != 0 ? "(Retorna ao início)":"[]";
 
+        return strRetorno;
+    }
 }
---
+++ src/main/java/one/digitalinnovation/Main.java
@@ -0,0 +1,10 @@
+package one.digitalinnovation;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircular<String> minhaListaCircular = new ListaCircular<>();
+
+        minhaListaCircular.add("c0");;
+        System.out.println(minhaListaCircular);
+    }
+}
